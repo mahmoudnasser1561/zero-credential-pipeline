634e29361fdc23a9660c56a51452567059c72891
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-07 11:13:38 +0300

    Extract shared data sources into data.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
